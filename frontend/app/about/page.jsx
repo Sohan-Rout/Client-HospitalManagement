2b5856da7e1140e56bf499153b048a150d2e6364
Sohan Rout <sohanrout84@gmail.com>
2026-04-16 21:03:25 +0530

    Feat : added edits to about page

--- frontend/app/about/page.jsx
+++ frontend/app/about/page.jsx
@@ -1,10 +1,25 @@
 import Navbar from "../../components/ui/navbar";
+import AboutHero from "../../components/about/hero";
+import AboutUs from "../../components/about/aboutUs";
+import Advantage from "../../components/home/advantage"
 
 export default function Page(){
     return(
         <main>
             <div className="py-6">
                 <Navbar />
+            </div>
+
+            <div className="py-8 max-w-6xl mx-auto">
+                <AboutHero />
+            </div>
+
+            <div className="max-w-6xl mx-auto py-8">
+                <AboutUs />
+            </div>
+
+            <div className="max-w-6xl mx-auto py-24">
+                <Advantage />
             </div>
         </main>
     );
